3ee0528001cb299d6665fa3415fd9dda97638aa4
IndikaKuma <ipkumarawd@yahoo.com>
2021-01-21 13:21:03 +0100

    Fixes for sonar cloud

--- tosca/api/src/main/java/nl/jads/tosca/restapi/YamlTesting.java
+++ tosca/api/src/main/java/nl/jads/tosca/restapi/YamlTesting.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 public class YamlTesting {
+    private static final java.util.logging.Logger log = Logger.getLogger(YamlTesting.class.getName());
+
     public static void main(String[] args) {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         try {
@@ -13,8 +16,7 @@ public class YamlTesting {
                     new File("C:\\Postdoc\\projects\\sodalite-eu\\defect-prediction\\tosca\\core\\src\\test\\resources\\hpc-clinical-uc\\service.yml"), TOSCADef.class);
             System.out.println(toscaDef.getMetadata().getTemplateName());
         } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            log.warning(e.getMessage());
         }
     }
 }

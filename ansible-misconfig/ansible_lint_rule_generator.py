1cbf159bddfd947eef2932af8f870107b907f9a8
IndikaKuma <ipkumarawd@yahoo.com>
2021-12-06 00:05:51 +0100

    removing misconfig detector code (to be replaced)

--- ansible-misconfig/ansible_lint_rule_generator.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('octal_template.txt')
-output_from_parsed_template = template.render(module_name='file', parameter_name='mode')
-print(output_from_parsed_template)
-
-# to save the results
-with open("rules/octal_file.py", "w") as fh:
-    fh.write(output_from_parsed_template)

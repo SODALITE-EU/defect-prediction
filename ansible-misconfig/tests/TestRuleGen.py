d9d71d772c087a5c74b17c461106638a074cd7bb
IndikaKuma <ipkumarawd@yahoo.com>
2022-01-27 16:35:49 +0100

    updated testcase

--- ansible-misconfig/tests/TestRuleGen.py
+++ ansible-misconfig/tests/TestRuleGen.py
@@ -13,8 +13,8 @@ class TestLongStatement(unittest.TestCase):
 
     def test_file(self):
         top_module_df, list_all_modules = scrap_module_info(modules_list=['copy'])
-        print(top_module_df)
-        print(list_all_modules)
+        assert top_module_df is not None
+        assert list_all_modules is not None
 
         # file_name = 'testResources/ansible-smell/many.yaml'
         # good_runner = Runner(file_name, rules=self.collection)
